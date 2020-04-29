5d5bc65184810b0a424102e4532fd554b9492303
Ifeoluwa  Akinremi-Wade <ifeoluwa.wade@gmail.com>
2020-04-29 21:45:25 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,14 @@
 class Dog
-  def initialize(name, breed)
+  def initialize(name, breed = "Mutt")
     @name = name
     @breed = breed
   end
 
-  def name
-    @name
+  def name=(name)
+    @name = name
+  end
+
+  def breed=(breed)
+    @breed
   end
 end
